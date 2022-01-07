164941397b605f91c0a8913f6948b4bfe8d8965e
Megidd Git <megiddgit@gmail.com>
2022-01-07 16:57:40 +0330

    Finally, get an answer with w3 assumed as 7:
    [(19/4, 57/4 - w3, w3, -33687/(400*w3 - 5700), 33687/(400*w3))]
    a1= 84.2175000000000
    a2= 84.2175000000000
    a3= 84.2175000000000
    a4= 84.2175000000000
    at= 336.87
    w1= 4.75000000000000
    w2= 7.25000000000000
    w3= 7.00000000000000
    h2= 11.6162068965517
    h3= 12.0310714285714

--- calc.py
+++ calc.py
@@ -6,16 +6,16 @@ tan = 8.89/19
 h1 = 17.73
 wt = 19
 
-a1 = h1*w1+(w1*w1*tan)/2
-a2 = h2*w2+(w2*w2*tan)/2
-a3 = h3*w3+(w3*w3*tan)/2
-a4 = w2*(h1+w1*tan-h2)+w3*(h1+(w1+w2)*tan-h3)
-at = (h1*wt)+(wt*wt*tan)/2
+a1 = h1*w1
+a2 = h2*w2
+a3 = h3*w3
+a4 = w2*(h1-h2)+w3*(h1-h3)
+at = (h1*wt)
 
-eq1 = a1 - a2
-eq2 = a1 - a3
-eq3 = a1 - a4
-eq4 = a1+a2+a3+a4-at
+eq1 = a1 - at/4
+eq2 = a2 - at/4
+eq3 = a3 - at/4
+eq4 = a4 - at/4
 eq5 = w1+w2+w3-wt
 
 eq1 = nsimplify(eq1, rational=1)
@@ -24,10 +24,17 @@ eq3 = nsimplify(eq3, rational=1)
 eq4 = nsimplify(eq4, rational=1)
 eq5 = nsimplify(eq5, rational=1)
 
-answer = nsolve((eq1, eq2, eq3, eq4, eq5), (w1, w2, w3, h2, h3), (6, 7, 8, 9, 10))
+answer = solve((eq1, eq2, eq3, eq4, eq5), (w1, w2, w3, h2, h3))
 print(answer)
-print('a1=', a1.evalf(subs=answer))
-print('a2=', a2.evalf(subs=answer))
-print('a3=', a3.evalf(subs=answer))
-print('a4=', a4.evalf(subs=answer))
+w3_ = 7
+assume_w3 = {w1: 19/4, w2: 57/4 - w3_, w3: w3_, h2: -33687/(400*w3_ - 5700), h3: 33687/(400*w3_)}
+print('a1=', a1.evalf(subs=assume_w3))
+print('a2=', a2.evalf(subs=assume_w3))
+print('a3=', a3.evalf(subs=assume_w3))
+print('a4=', a4.evalf(subs=assume_w3))
 print('at=', at)
+print('w1=', w1.evalf(subs=assume_w3))
+print('w2=', w2.evalf(subs=assume_w3))
+print('w3=', w3.evalf(subs=assume_w3))
+print('h2=', h2.evalf(subs=assume_w3))
+print('h3=', h3.evalf(subs=assume_w3))
