b8cbf7315db3f1e05930943e7d5790ae69ce9025
Megidd Git <megiddgit@gmail.com>
2022-01-07 15:09:19 +0330

    Numeric solver: ZeroDivisionError
    matrix is numerically singular

--- calc.py
+++ calc.py
@@ -10,7 +10,13 @@ a3 = h3*w3+(w3*w3*tan)/2
 a4 = w2*(17.73+w1*tan-h2)+w3*(17.73+w1*tan+w2*tan-h3)
 at = (17.73*19)+(8.89*19)/2
 
-answer = solve([Eq(a1,a2), Eq(a1,a3), Eq(a1,a4), Eq(a1+a2+a3+a4,at), Eq(w1+w2+w3,19)], [w1, w2, w3, h2, h3])
+eq1 = a1 - a2
+eq2 = a1 - a3
+eq3 = a1 - a4
+eq4 = a1+a2+a3+a4-at
+eq5 = w1+w2+w3-19
+
+answer = nsolve((eq1, eq2, eq3, eq4, eq5), (w1, w2, w3, h2, h3), (6, 6, 7, 9, 10), verify=False)
 print(answer)
 print('a1=', a1.evalf(subs=answer))
 print('a2=', a2.evalf(subs=answer))
